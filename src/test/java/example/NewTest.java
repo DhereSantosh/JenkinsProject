fe395f2e6c782208ba97add0dfb12e61dbe19d97
Amar <Amar@DESKTOP-PGN0CGC>
2020-10-22 18:22:15 +0530

    my first commit

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;	
+	
+  @Test
+  public void testEasy() {
+	  driver.get("http://demo.guru99.com/test/guru99home/");  
+	  String title = driver.getTitle();				 
+	  Assert.assertTrue(title.contains("Demo Guru99 Page")); 
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.gecko.driver", "D:\\Selenium -BrowserDrivers\\Drivers\\Mozilla Firfox\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+	  driver = new FirefoxDriver();  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
